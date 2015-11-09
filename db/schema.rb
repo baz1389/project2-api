f547ab709e4e5945d84bf556ecc25dfc8104ddc4
Ben <benjamin.a.spencer@gmail.com>
2015-11-09 11:18:07 -0500

    updated schema with words foreign key to user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806195336) do
+ActiveRecord::Schema.define(version: 20151109160232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,4 +35,16 @@ ActiveRecord::Schema.define(version: 20150806195336) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
+  create_table "words", force: :cascade do |t|
+    t.string   "name"
+    t.text     "definition"
+    t.text     "sample_sentence"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "user_id"
+  end
+
+  add_index "words", ["user_id"], name: "index_words_on_user_id", using: :btree
+
+  add_foreign_key "words", "users"
 end
