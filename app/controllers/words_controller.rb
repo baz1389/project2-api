b6cb06f023286f944c6472e84ebc47ec05cfe60f
Ben <benjamin.a.spencer@gmail.com>
2015-11-09 13:11:52 -0500

    adds words controller. Index and Show working. Having trouble with Token Auth to test remaining methods (create, update, destroy)

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,22 +1,39 @@
-class WordsController < ApplicationController
+class WordsController < OpenReadController
   def index
-
+    render json: Word.all
   end
 
   def show
-
+    render json: Word.find(params[:id])
   end
 
   def create
-
+    word = Word.create(word_params)
+    if word.save
+      render json: word, status: :created
+    else
+      render json: word.errors, status: :unprocessable_entity
+    end
   end
 
   def update
-
+    word = Word.find(params[:id])
+    if word.update_attributes(word_params)
+      render json: word, status: :update
+    else
+      render json: word.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    Word.find(params[:id]).destroy
+    head :ok
+  end
 
+  private
+
+  def word_params
+    params.require(:word).permit([:name, :definition, :sample_sentence, :user_id])
   end
 
 end
